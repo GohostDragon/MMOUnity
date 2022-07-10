29b577c482664af1c963cf5625dcce92221f17c0
MecaDragon <kvg753@gmail.com>
2022-07-11 02:19:04 +0900

    플레이어 회전, Input Manager 추가 회전은 쿼터니언 이용 키 이벤트는 Input Manager에서 관리

--- MMO_Unity/Assets/Scripts/Managers/Managers.cs
+++ MMO_Unity/Assets/Scripts/Managers/Managers.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class Managers : MonoBehaviour
 {
     static Managers s_Instance; // 유일성이 보장된다
-    public static Managers Instance { get { Init(); return s_Instance; } } // 유일한 매니저를 갖고온다
+    static Managers Instance { get { Init(); return s_Instance; } } // 유일한 매니저를 갖고온다
+
+    InputManager _input = new InputManager();
+    public static InputManager Input { get { return Instance._input; } }
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +18,7 @@ public class Managers : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        _input.OnUpdate();
     }
 
     static void Init()
--- MMO_Unity/Assets/Scripts/PlayerController.cs
+++ MMO_Unity/Assets/Scripts/PlayerController.cs
@@ -2,84 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// 1. 위치 벡터
-// 2. 방향 벡터
-struct MyVector
-{
-    public float x;
-    public float y;
-    public float z;
-
-    public MyVector(float x, float y, float z) { this.x = x; this.y = y; this.z = z; }
-
-    public float magnitude { get { return Mathf.Sqrt(x*x + y*y + z*z); } }
-    public MyVector normalized { get { return new MyVector(x / magnitude, y / magnitude, z / magnitude); } }
-    public static MyVector operator +(MyVector a, MyVector b)
-    {
-        return new MyVector(a.x + b.x, a.y + b.y, a.z + b.z);
-    }
-
-    public static MyVector operator -(MyVector a, MyVector b)
-    {
-        return new MyVector(a.x - b.x, a.y - b.y, a.z - b.z);
-    }
-
-    public static MyVector operator *(MyVector a, float d)
-    {
-        return new MyVector(a.x * d, a.y * d, a.z * d);
-    }
-}
-
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     float _speed = 10.0f;
     void Start()
     {
-        MyVector to = new MyVector(10.0f, 10.0f, 0.0f);
-        MyVector from = new MyVector(5.0f, 10.0f, 0.0f);
-        MyVector dir = to - from; // (5.0f, 0.0f, 0.0f)
-
-        dir = dir.normalized; // (1.0f, 0.0f, 0.0f)
-
-        MyVector newPos = from + dir * _speed;
-
-        // 방향 벡터
-            // 1. 거리(크기)    magnitude
-            // 2. 실제 방향
+        Managers.Input.KeyAction -= OnKeyboard;
+        Managers.Input.KeyAction += OnKeyboard;
     }
 
-    // GameObject (Player)
-        // Transform
-        // PlayerController (*)
-
+    float _yAngle = 0.0f;
     void Update()
     {
-        // Local -> World
-        // TransformDirection
 
-        // World -> Local
-        // InverseTransformDirection
+        //절대 회전값
+        //transform.eulerAngles = new Vector3(0.0f, _yAngle, 0.0f);
 
-        /*
+        // +- delta
+        //transform.Rotate(new Vector3(0.0f, Time.deltaTime * 100.0f, 0.0f));
+
+        //transform.rotation = Quaternion.Euler(new Vector3(0.0f, _yAngle, 0.0f));
+
+        //해당 방향 벡터로 방향 돌림
+        //transform.rotation = Quaternion.LookRotation(Vector3.forward);
+    }
+
+    void OnKeyboard()
+    {
         if (Input.GetKey(KeyCode.W))
-            transform.position += transform.TransformDirection(Vector3.forward * Time.deltaTime * _speed);
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
+            transform.position += Vector3.forward * Time.deltaTime * _speed;
+        }
         if (Input.GetKey(KeyCode.S))
-            transform.position += transform.TransformDirection(Vector3.back * Time.deltaTime * _speed);
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
+            transform.position += Vector3.back * Time.deltaTime * _speed;
+        }
         if (Input.GetKey(KeyCode.A))
-            transform.position += transform.TransformDirection(Vector3.left * Time.deltaTime * _speed);
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
+            transform.position += Vector3.left * Time.deltaTime * _speed;
+        }
         if (Input.GetKey(KeyCode.D))
-            transform.position += transform.TransformDirection(Vector3.right * Time.deltaTime * _speed);
-        */
-
-        if (Input.GetKey(KeyCode.W))
-            transform.Translate(Vector3.forward * Time.deltaTime * _speed);
-        if (Input.GetKey(KeyCode.S))
-            transform.Translate(Vector3.back * Time.deltaTime * _speed);
-        if (Input.GetKey(KeyCode.A))
-            transform.Translate(Vector3.left * Time.deltaTime * _speed);
-        if (Input.GetKey(KeyCode.D))
-            transform.Translate(Vector3.right * Time.deltaTime * _speed);
-        //transform
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
+            transform.position += Vector3.right * Time.deltaTime * _speed;
+        }
     }
 }
